1c9a858f6adfd4d18c74549f8ee882d9f465d6ac
ww23e02 <ww23e022@technikum.wien.at>
2024-05-26 23:18:11 +0200

    Initial commit

---
+++ src/test/java/fh/slm/weathermonitor/ApplicationTests.java
@@ -0,0 +1,13 @@
+package fh.slm.weathermonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
